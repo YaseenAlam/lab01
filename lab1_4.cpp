b9e7d35e371d94d721a8812ddc38608522193421
Yaseen Alam <yaseenalamm@hotmail.com>
2022-09-05 09:45:42 -0700

    first commit

---
+++ lab1_4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class point
+{
+ public:
+  int x;
+  int y;
+  float distance()
+    {
+     return sqrt(x*x + y*y);
+    }
+};
+
+int main()
+{
+ point* p = new point();
+ p->x = 14;
+ p->y = 63;
+ cout << "(" << p->x<< ", " << p->y<< ")" << endl;
+ cout << p->distance()<< endl;
+ return 0;
+}
